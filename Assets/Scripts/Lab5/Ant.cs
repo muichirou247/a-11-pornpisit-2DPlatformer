0181864384e8d2b5025a8cd5ca5e9c50826ede1a
muichirou247 <pornpisit.ratt@bumail.net>
2024-10-01 10:36:28 +0700

    AntComplete

--- Assets/Scripts/Lab5/Ant.cs
+++ Assets/Scripts/Lab5/Ant.cs
@@ -1,14 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class Ant : Enemy
 {
-    private Vector2 velocity;
-    private Transform[] movePoints;
+    [SerializeField] private Vector2 velocity;
+    [SerializeField] private Transform[] movePoints;
+
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        Init(15);   
+        Debug.Log($"ant = {Health}");
+
+        Behavior();
+        }
+
+        private void FixedUpdate()
+        {
+        Behavior();
+        }
+        
 
     public override void Behavior()
-    {
-        Debug.Log("Ant");
-    }
+{
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (rb.position.x <= movePoints[0].position.x && velocity.x < 0)
+        {
+            Flip();
+        }
+
+        if (rb.position.x >= movePoints[1].position.x && velocity.x > 0)
+        {
+            Flip();
+        }
+
+           }
+        public void Flip()
+        {
+        velocity.x *= -1;
+
+        Vector2 charScale = transform.localScale;
+        charScale.x *= -1;
+        transform.localScale = charScale;
+        }
+
 }
+
--- Assets/Scripts/Lab5/Croccodile.cs
+++ Assets/Scripts/Lab5/Croccodile.cs
@@ -6,9 +6,37 @@ public class Croccodile : Enemy
 {
     private float attackRange;
     private Player player;
+    private float waitTime;
+    private float reloadTime;
+    private int damageHit;
+    private Animator animator;
+
+
+    public void Start()
+    {
+        Init(50);
+        waitTime = 0.0f;
+        reloadTime = 5.0f;
+        damageHit = 30;
+        attackRange = 6;
+        Debug.Log($"Croccodile HP = {Health}");
+
+        Behavior();
+    }
     public override void Behavior()
     {
-        Debug.Log("Choccodile");
+        Vector2 distance = player.transform.position - transform.position;
+        if(distance.magnitude <= attackRange)
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        if (waitTime >= reloadTime)
+        {
+            animator;
+        }
     }
    
 }
